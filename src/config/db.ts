7994e23d15205b8507dac4cadb871019dfbf31da
Enrique Comesaña <enriquecbar@gmail.com>
2024-05-31 01:30:17 +0200

    process env mod

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,10 @@
 import { Sequelize } from 'sequelize-typescript';
 
-process.loadEnvFile('./.env.local');
 
-const db = new Sequelize(process.env.DATABASE_URL, {
+
+process.loadEnvFile('../../.env.local');
+
+const db = new Sequelize(process.env.DATABASE_URL!, {
   models: [__dirname + '/../models/**/*'],
   logging: false,
   dialectOptions: {
