ae7f8da76b5d3b2ff55c8dddff4fc1cb411f5b1b
Enrique Comesaña <enriquecbar@gmail.com>
2024-05-30 21:37:04 +0200

    ssl api

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,9 +2,14 @@ import { Sequelize } from 'sequelize-typescript';
 
 process.loadEnvFile('./.env.local');
 
-const db = new Sequelize(process.env.DATABASE_URL,{
-    models: [__dirname + '/../models/**/*'],
-    logging: false
+const db = new Sequelize(process.env.DATABASE_URL, {
+  models: [__dirname + '/../models/**/*'],
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true
+    }
+  }
 });
 
 export default db;
