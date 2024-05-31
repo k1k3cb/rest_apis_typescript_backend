34461a409cf448705a758b767e27058681ea5c99
Enrique Comesaña <enriquecbar@gmail.com>
2024-06-01 00:35:38 +0200

    dotenv install

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,7 @@
+import * as dotenv from 'dotenv';
 import { Sequelize } from 'sequelize-typescript';
-
-
-
-process.loadEnvFile('../../.env.local');
+// process.loadEnvFile('./.env.local');
+dotenv.config({ path: './.env.local' });
 
 const db = new Sequelize(process.env.DATABASE_URL!, {
   models: [__dirname + '/../models/**/*'],
